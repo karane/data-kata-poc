50e3cf8f84da396ca1fcf098c749011577a2dd4b
Karane Vieira <karane@gmail.com>
2026-03-18 00:28:12 -0300

    two postgres instances

--- db-flink-batch-poc/submit.sh
+++ db-flink-batch-poc/submit.sh
@@ -36,10 +36,10 @@ docker run --rm \
   --network db-flink-batch-poc_default \
   -v "$JAR":/job.jar \
   -e RUSTFS_BUCKET=sales-csv \
-  -e SOURCE_DB_URL=jdbc:postgresql://postgres:5432/salesdb \
+  -e SOURCE_DB_URL=jdbc:postgresql://postgres-source:5432/salesdb \
   -e SOURCE_DB_USER=poc \
   -e SOURCE_DB_PASS=poc123 \
-  -e SINK_DB_URL=jdbc:postgresql://postgres:5432/salesdb \
+  -e SINK_DB_URL=jdbc:postgresql://postgres-sink:5432/salesdb \
   -e SINK_DB_USER=poc \
   -e SINK_DB_PASS=poc123 \
   flink:1.18-java11 \
